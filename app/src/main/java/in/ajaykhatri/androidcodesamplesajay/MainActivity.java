36f5b9337ce20a30c359495ac06b14cfbc40c7e4
Ajay Khatri <ajay.acropolis@gmail.com>
2017-07-28 06:07:31 +0530

    first upload test

---
+++ app/src/main/java/in/ajaykhatri/androidcodesamplesajay/MainActivity.java
@@ -0,0 +1,21 @@
+package in.ajaykhatri.androidcodesamplesajay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void TestGit(){
+        int i = 1+1;
+
+        int j = 2+2;
+    }
+
+
+}
